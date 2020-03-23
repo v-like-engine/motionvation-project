8d48eeccef34672de22e69693b078a6d0d72d456
mihdenis85 <mihdenis777@yandex.ru>
2020-03-23 18:23:38 +0500

    Add running app

--- Project/application.py
+++ Project/application.py
@@ -1,3 +1,4 @@
+import os
 from datetime import timedelta
 
 from flask import Flask, render_template, redirect, url_for
@@ -5,3 +6,8 @@ from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key_Denis_and_VLAD7879845465465489794546___'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=365)
+
+
+def run():
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
