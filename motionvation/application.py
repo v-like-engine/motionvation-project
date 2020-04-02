c445e2d20e20f37056ffaef7c62301ea68e3b73c
mihdenis85 <mihdenis777@yandex.ru>
2020-04-02 20:47:31 +0500

    Add login route

--- motionvation/application.py
+++ motionvation/application.py
@@ -3,19 +3,24 @@ import random
 from datetime import timedelta
 
 from flask import Flask, render_template, redirect, url_for
+from flask_login import LoginManager, login_user
+
+from motionvation.data import db_session
+from motionvation.data.models.users import User
+from motionvation.forms.login_form import LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key_Denis_and_VLADiSLav___'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=365)
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     db = db_session.create_session()
-#     return db.query(User).get(user_id)
+@login_manager.user_loader
+def load_user(user_id):
+    db = db_session.create_session()
+    return db.query(User).get(user_id)
 
 
 @app.route('/planger')
@@ -45,6 +50,23 @@ def challenge():
     for i in range(random.randint(1, 10)):
         challenges.append(random.choice(task) + str(random.randint(5, 50)))
     return render_template('challenges.html', title='Challenges', chs=challenges)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        db = db_session.create_session()
+        user = db.query(User).filter(User.email == login_form.email.data).first()
+        if not user:
+            return render_template('login.html', form=login_form, message="Такого пользователя не существует")
+        if user.check_password(login_form.password.data):
+            login_user(user, remember=login_form.remember_me.data)
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', form=login_form, message="Неверный пароль")
+    else:
+        return render_template('login.html', form=login_form)
 
 
 @app.route('/nothing')
---
+++ motionvation/forms/__init__.py
@@ -0,0 +1,5 @@
+from motionvation.forms.resgister_form import RegisterForm
+from motionvation.forms.login_form import LoginForm
+
+
+__all__ = ['LoginForm', 'RegisterForm']
