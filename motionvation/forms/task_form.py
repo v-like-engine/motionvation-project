940facceb064eb7f080b96746e5ac406439473f4
Vladislav <cypepv@yandex.ru>
2020-04-12 15:34:19 +0000

    Change the priority constants

--- motionvation/forms/task_form.py
+++ motionvation/forms/task_form.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length
 
 
 class TaskForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    description = TextAreaField('Description', validators=[DataRequired(), Length(max=600, message='Maximum 600 symbols')])
-    priority = SelectField('Priority', choices=[('High', 'High'), ('Normal', 'Normal'), ('Low', 'Low')])
+    title = StringField('Title', validators=[DataRequired(), Length(max=100, message='Maximum 100 symbols')])
+    description = TextAreaField('Description', validators=[DataRequired(), Length(max=200, message='Maximum 200 symbols')])
+    priority = SelectField('Priority', choices=[('10', '10'), ('9', '9'), ('8', '8'), ('7', '7'), ('6', '6'), ('5', '5'), ('4', '4'), ('3', '3'), ('2', '2'), ('1', '1')], default='5')
     submit = SubmitField('Add')
