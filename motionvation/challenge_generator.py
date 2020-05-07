57b5d5f1cac0d1a2dc7f7f11a35d0bb23107efac
v-like-engine <cypepv@yandex.ru>
2020-05-07 15:53:20 +0500

    Add two new bgs and refactor generator of challenges

--- motionvation/challenge_generator.py
+++ motionvation/challenge_generator.py
@@ -2,57 +2,58 @@ import random
 
 
 def choose_challenge_plot():
-    n = random.randint(0, 99)
-    if n < 59:
-        plot = [random.choice(VARIANTS[:7])]
-    elif n < 80:
-        plot = random.choice([VARIANTS[:2], VARIANTS[2:4], VARIANTS[4:6]])
-    elif n < 95:
-        plot = [VARIANTS[-2]]
-    elif n <= 99:
-        plot = VARIANTS[:7]
+    chance = random.randint(0, 99)
+    if chance < 59:
+        plot = [random.choice(CHALLENGE_TASK_VARIANTS[:7])]
+    elif chance < 80:
+        plot = random.choice([CHALLENGE_TASK_VARIANTS[:2], CHALLENGE_TASK_VARIANTS[2:4], CHALLENGE_TASK_VARIANTS[4:6]])
+    elif chance < 95:
+        plot = [CHALLENGE_TASK_VARIANTS[-2]]
+    elif chance <= 99:
+        plot = CHALLENGE_TASK_VARIANTS[:6]
     else:
-        plot = [VARIANTS[-1]]
+        plot = [CHALLENGE_TASK_VARIANTS[-1]]
     return plot
 
 
 def count_plot(plot):
-    c, diff, step2, step1 = 0, 0, 0, 0
+    count_of_points_required, diff, points_for_hard_diff, points_for_medium_diff = 0, 0, 0, 0
     if not isinstance(plot, list):
         plot = [plot]
     if len(plot) == 1:
-        if plot[0] < 6:
-            c = random.randint(1, 10)
+        if plot[0] < do_challenge:
+            count_of_points_required = random.randint(1, 10)
             if plot[0] not in [2, 3, 4]:
-                step2, step1 = 9, 6
+                points_for_hard_diff, points_for_medium_diff = 9, 6
             else:
-                step2, step1 = 10, 8
-        elif plot[0] < 7:
-            c = random.randint(1, 3)
-            step2, step1 = 2, 1
-        elif plot[0] == 7:
-            c = random.randint(4, 40) * 5
-            step2, step1 = 320, 200
-        elif plot[0] == 8:
-            c = random.randint(1, 5)
-            step2, step1 = 3, 1
+                points_for_hard_diff, points_for_medium_diff = 10, 8
+        elif plot[0] < get_xp:
+            count_of_points_required = random.randint(1, 3)
+            points_for_hard_diff, points_for_medium_diff = 2, 1
+        elif plot[0] == get_xp:
+            count_of_points_required = random.randint(4, 40) * 5
+            points_for_hard_diff, points_for_medium_diff = 320, 200
+        elif plot[0] == get_level:
+            count_of_points_required = random.randint(1, 5)
+            points_for_hard_diff, points_for_medium_diff = 3, 1
     elif len(plot) == 2:
-        if 3 not in plot:
-            c = random.randint(2, 25)
-            step2, step1 = 17, 12
+        if delete_task not in plot:
+            count_of_points_required = random.randint(2, 25)
+            points_for_hard_diff, points_for_medium_diff = 17, 12
         else:
-            c = random.randint(2, 18)
-            step2, step1 = 17, 10
+            count_of_points_required = random.randint(2, 18)
+            points_for_hard_diff, points_for_medium_diff = 17, 10
     else:
-        c = random.randint(5, 60)
-        step2, step1 = 50, 25
-    return c, calculate_difficulty(c, step2, step1)
+        count_of_points_required = random.randint(5, 60)
+        points_for_hard_diff, points_for_medium_diff = 50, 25
+    return count_of_points_required, calculate_difficulty(count_of_points_required,
+                                                          points_for_hard_diff, points_for_medium_diff)
 
 
-def calculate_difficulty(c, step2, step1):
-    if c > step2:
+def calculate_difficulty(count_of_points_required, points_for_hard_diff, points_for_medium_diff):
+    if count_of_points_required > points_for_hard_diff:
         diff = 2
-    elif c > step1:
+    elif count_of_points_required > points_for_medium_diff:
         diff = 1
     else:
         diff = 0
@@ -65,16 +66,16 @@ def generate_challenge():
     if len(plot) > 2:
         text = 'Do actions on site: '
     elif len(plot) == 2:
-        if 1 in plot:
+        if add_task in plot:
             text = 'Add tasks or notes: '
-        elif 3 in plot:
+        elif delete_task in plot:
             text = 'Delete tasks or notes: '
-        elif 5 in plot:
+        elif do_task in plot:
             text = 'Do tasks or challenges: '
         else:
             text = 'Do some kind of action: '
     else:
-        text = TEXTS[plot[0] - 1]
+        text = TITLES_OF_CHALLENGES[plot[0] - 1]
     text += str(required)
     return {'plot': plot, 'required': required, 'difficulty': difficulty, 'text': text}
 
@@ -88,6 +89,6 @@ do_challenge = 6
 get_xp = 7
 get_level = 8
 
-VARIANTS = [add_task, add_note, delete_task, delete_note, do_task, do_challenge, get_xp, get_level]
-TEXTS = ['Add tasks: ', 'Add notes: ', 'Delete tasks: ', 'Delete notes: ', 'Do tasks: ',
-         'Win challenges: ', 'Get experience: ', 'Get levels: ']
+CHALLENGE_TASK_VARIANTS = [add_task, add_note, delete_task, delete_note, do_task, do_challenge, get_xp, get_level]
+TITLES_OF_CHALLENGES = ['Add tasks: ', 'Add notes: ', 'Delete tasks: ', 'Delete notes: ', 'Do tasks: ',
+                        'Win challenges: ', 'Get experience: ', 'Get levels: ']
